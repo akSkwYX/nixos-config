a438a0d0c1f31e93f45b88d9828e39a4ac02a0ad
akSkwYX <akskwyx@gmail.com>
2025-06-08 12:22:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# My NixOS configuration
+
+I'm trying to get it as reproducible as possible but I'm a beginner
+
+in both nixos and linux so I may made many mistakes, and this configuration
+
+couldn't be as flexible as I would want it to be, yet I think that if you
+
+just git clone this repository and rebuild after with the command :
+
+sudo nixos-rebuild switch --flake .#default
+
+after going in the repo folder, it should work.
+
+Yet I don't test it for the moment but will certainly do it soon enough
---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "NixOs + HomeManager configuration flake";
+
+  inputs = {
+    # Nixpkgs, home manager and flake utils
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    # Dotfiles flake
+    neovim-flake.url = "github:akSkwYX/neovim-dotfiles";
+    hypr-flake.url = "github:akSkwYX/hypr-dotfiles";
+  };
+
+  outputs = { self, nixpkgs, home-manager, flake-utils, neovim-flake, hypr-flake, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      nixosConfigurations.default = pkgs.lib.nixosSystem {
+        inherit system;
+
+        modules = [
+          # Hardware detection
+          ./hosts/hardware-configuration.nix
+
+          # Core system modules
+          ./modules/system/basic-packages.nix
+          ./modules/system/networking.nix
+          
+          # Per-host overrides
+          ./hosts/default.nix
+          
+          # Integrate home-manager
+          home-manager.nixosModules.home-manager
+
+          # Configure the user home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.skwyx = import ./hosts/home.nix;
+          }
+        ];
+      };
+    }
+  );
+}
