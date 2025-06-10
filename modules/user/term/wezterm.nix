166277605f0fb9950e5fff4e94559ff7473f0d8e
akSkwYX <akskwyx@gmail.com>
2025-06-10 16:59:07 +0200

    Correction of some bugs

--- modules/user/term/wezterm.nix
+++ modules/user/term/wezterm.nix
@@ -14,6 +14,12 @@ config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = true
 config.window_background_opacity = 0.8
+config.window_padding = {
+  left = \"0px\",
+  right = \"0px\",
+  top = \"0px\",
+  bottom = \"0px\",
+}
 
 return config
 ";
